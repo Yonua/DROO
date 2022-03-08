46cfb734c0386e58efe0bb0ea5d0978b1a32a9fc
euphoria2022 <2057035911@qq.com>
2022-03-08 09:48:22 +0800

    🪲 notes

--- Besection.jl
+++ Besection.jl
@@ -1,5 +1,6 @@
 module Besection
 using LambertW
+
 function phi(v, j, wj, epsilon)
     return 1 / (-1 - 1 / (real(lambertw(-1 / (exp(1 + v / wj[j] / epsilon))))))
 end
@@ -35,23 +36,25 @@ function rate(x,wi,wj,hi,hj,ki,eta1,eta2,M1,epsilon)
 end
 
 function besection(h::Vector, M::Vector, weights::Vector=[])
+    # 根据信道增益与卸载策略计算无线射频时间与个设备分配到的资源以及加权和计算速率
     # some parameters
     o = 100
-    p = 3
-    u = 0.7
-    eta1 = ((u * p)^(1.0 / 3)) / o
-    ki = 10^-26
-    eta2 = u * p / 10^-10
-    B = 2 * 10^6
+    p = 3                                  # 发射功率
+    u = 0.7                                # 设备接受能量的效率
+    eta1 = ((u * p)^(1.0 / 3)) / o         # 公式中出现的常量
+    ki = 10^-26                            
+    eta2 = u * p / 10^-10                  # 公式中出现的常量
+    B = 2 * 10^6                           # 
     Vu = 1.1
     epsilon = B / (Vu * log(2))
-    x = Vector{Float32}()
+    x = Vector{Float32}()                  # 各设备分配到的资源（时间），第一个元素用于存加权和速率
 
     # offloading to locally, offloading to edge
-    M0, M1 = findall(x -> x == 0, M), findall(x -> x == 1, M)
-    hi, hj = h[M0], h[M1]
+    M0, M1 = findall(x -> x == 0, M), findall(x -> x == 1, M) # 获取本地计算与边缘计算设备列表
+    hi, hj = h[M0], h[M1]                                     # 分别获取本地计算与卸载到边缘计算的信道增益
 
     N = length(M)
+    # 各设备的权重
     if length(weights) < N
         weights = ones(Float32, N)
         weights[1:2:N] .= 1.5
@@ -59,6 +62,7 @@ function besection(h::Vector, M::Vector, weights::Vector=[])
     wi = weights[M0]
     wj = weights[M1]
 
+    # 下面这一段没懂什么意思 主要求解出加权和速率和资源的分配
     delta = 0.005
     UB = 999999999
     LB = 0
@@ -79,6 +83,7 @@ function besection(h::Vector, M::Vector, weights::Vector=[])
 end
 
 function knm(m,k=1)
+    # 论文当中提到的公式  保序法
     m_list = []
     push!(m_list,1*(m.>0.5))
     if k > 1
--- main.jl
+++ main.jl
@@ -4,19 +4,24 @@ using MAT
 using Random
 using TensorBoardLogger,Logging
 
+# 结果显示-与python pytorch的tensorboard一样
 lg=TBLogger("tensorboard_logs/run", min_level=Logging.Info)
 
-# besection([1.0,2,3],[1,0,1],[1.0])
-
-# defind variables
+# defind some variables
+# 内存大小-存储数据条数
 const MEMORY_SIZE = 1024
+# 用户设备数量
 const DEVICE_NUM = 10
+# 优化间隔-DNN的训练间隔
 const OPTIM_INTERVAL = 32
+# 一次投入数据大小
 const BATCH_SIZE = 128
+# DNN学习率  ADAM
 const LEARNING_RATE = 0.01
 const LEARNING_BETAS = (0.09,0.999)
 
 # defind module
+# DNN网络
 DNN = Chain(
     x->transpose(x),
     Dense(DEVICE_NUM,120,relu),
@@ -24,47 +29,67 @@ DNN = Chain(
     Dense(80,DEVICE_NUM,sigmoid),
     x->transpose(x)
 )
-
+# 内存大小 2*DEVICE_NUM表示信道增益与对应的卸载策略的大小
 MEMORY = zeros(Float32,MEMORY_SIZE,2*DEVICE_NUM)
 
 # data handle
+# 导入数据
 INPUT_H = matread("data/data_10.mat")["input_h"].* 10^6
-
+# 损失函数  交叉熵损失
 loss(x,y) = Flux.Losses.binarycrossentropy(DNN(x),y)
+# 优化器
 optimizer = Flux.Optimise.ADAM(LEARNING_RATE,LEARNING_BETAS,0.0001)
+# 模型参数
 params = Flux.params(DNN)
 
 println("Start training")
 # training
 
 with_logger(lg) do
+    # 开始训练
     for epoch = 1:size(INPUT_H,1)
+        # 获取当前信道增益
         h = INPUT_H[epoch,:]
+        # 通过DNN获取卸载决策变量
         xi = DNN(Flux.unsqueeze(h,1))
+        # 使用保序法获取决策向量
         mlist = Besection.knm(xi[1,:],10)
+        # 获取每一个决策向量对应的计算速率
         r_list = []
         for m = mlist
             push!(r_list,Besection.besection(h/10^6,m)[1])
         end
+        # 将最大的速率对应的信道增益与卸载决策存入内存
         MEMORY[(epoch-1)%MEMORY_SIZE + 1,:] =  vcat(h,mlist[argmax(r_list)])
         # optimizer
+        # 达到优化间隔进行优化
         if epoch % OPTIM_INTERVAL == 0
+            # 判断当前时间帧是否超过内存大小
             if epoch < MEMORY_SIZE
+                # 没有，则从当前时间帧之前可重复选择出训练批次大小的下标
                 idx = rand(1:epoch,BATCH_SIZE)
             else
                 idx = randperm(MEMORY_SIZE)[1:BATCH_SIZE]
             end
+            # 从内存中读取数据
             hm = MEMORY[idx,:]
+            # 获取信道增益
             h_ = hm[:,1:DEVICE_NUM]
+            # 获取卸载决策
             m_ = hm[:,DEVICE_NUM+1:2*DEVICE_NUM]
+            # 损失变量
             local loss_v = 0.0
-            
+            # 计算梯度
             gs = gradient(params) do
+                # 计算损失
                 loss_val = loss(h_,m_)
                 loss_v = loss_val
+                # 返回损失
                 return loss_val
             end
+            # 更新参数  使用优化器与梯度
             Flux.update!(optimizer,params,gs)
+            # 简单绘制结果到tensorboard
             @info "DROO_LOSS" loss=loss_v step = epoch
         end
     end
